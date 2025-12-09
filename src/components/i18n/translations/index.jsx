b4a08ad69c7fb94f3078e6ad92eebe6a38953f6d
patrick van zandvoort <patrick.van.zandvoort@gmail.com>
2025-12-09 13:15:30 +0000

    File changes

---
+++ src/components/i18n/translations/index.jsx
@@ -0,0 +1,22 @@
+// Central translations index
+import { nl } from './nl';
+import { en } from './en';
+import { fr } from './fr';
+import { de } from './de';
+import { zhCN } from './zh-CN';
+
+export const translations = {
+  nl,
+  en,
+  fr,
+  de,
+  "zh-CN": zhCN
+};
+
+export const languageNames = {
+  nl: "Nederlands",
+  en: "English",
+  fr: "Français",
+  de: "Deutsch",
+  "zh-CN": "中文",
+};
